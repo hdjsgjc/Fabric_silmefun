45848a25b6302d72bd017d5c77b683e3230d9c86
t1234 <t12345fglj3@163.com>
2024-02-22 14:35:56 +0800

    g

--- src/main/java/com/example/ExampleModClient.java
+++ src/main/java/com/example/ExampleModClient.java
@@ -2,7 +2,7 @@ package com.example;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public class ExampleModClien implements ClientModInitializer {
+public class ExampleModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         // 在这里执行客户端初始化的操作
